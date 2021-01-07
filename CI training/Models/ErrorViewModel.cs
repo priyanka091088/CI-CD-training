fd79b390c1bb6d054cafeeb0ae3c15ec2fb1c574
priyanka091088 <priyanka@promactinfo.com>
2021-01-07 12:42:47 +0530

    first commit

---
+++ CI training/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CI_training.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
